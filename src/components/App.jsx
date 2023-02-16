d8e130d73d78539a5b74b0e3b4560dbb77e4045c
osyfliak <115475388+osyfliak@users.noreply.github.com>
2023-02-16 10:02:10 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,6 @@ import Contact from './Contact/Contact';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
       <Contact />
-    </div>
   );
 };
